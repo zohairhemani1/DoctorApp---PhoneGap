dcc5d5043f8028cbf168da78d3560423233baa4a
zohairHemani1 <zohairhemani1@gmail.com>
2015-09-07 20:01:01 +0500

    Commit.

---
+++ doctorApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Zohair Hemani\Desktop\doctorApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Zohair Hemani\Desktop\doctorApp\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ doctorApp/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Zohair Hemani\Desktop\doctorApp\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Zohair Hemani\Desktop\doctorApp\platforms\android\AndroidManifest.xml \
